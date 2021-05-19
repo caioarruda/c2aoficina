f03a0e389438373fa1fc0ddc1afa7c02e8f3c11f
caio arruda <caioarruda@gmail.com>
2021-05-19 20:11:35 -0300

    inicial

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'proxy-polyfill';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import dataProviderFactory from './dataProvider';
+import fakeServerFactory from './fakeServer';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/**
+ * This demo can work with either a fake REST server, or a fake GraphQL server.
+ *
+ * To avoid bundling both libraries, the dataProvider and fake server factories
+ * use the import() function, so they are asynchronous.
+ */
+const prepareDataProvider = async () => {
+    const restoreFetch = await fakeServerFactory(
+        process.env.REACT_APP_DATA_PROVIDER || ''
+    );
+    const dataProvider = await dataProviderFactory(
+        process.env.REACT_APP_DATA_PROVIDER || ''
+    );
+    return { dataProvider, restoreFetch };
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+prepareDataProvider().then(({ dataProvider, restoreFetch }) => {
+    ReactDOM.render(
+        <App dataProvider={dataProvider} onUnmount={restoreFetch} />,
+        document.getElementById('root')
+    );
+});
